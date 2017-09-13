d8895a6291c3869c921c576d5d673de207176003
MD5HashBrowns <md5hashbrownz@gmail.com>
2017-09-13 17:42:53 -0400

    added assets

---
+++ dist/img/nav.php
@@ -0,0 +1,19 @@
+<nav id="nav" class="large">
+  <div>
+    <a id="nav-logo" href="index.php"><img src="img/logo.jpg" alt="Alexander Cardosi Logo"></a>
+    <a href="index.php" title="Home" aria-hidden="true" role="presentation">Alexander Cardosi</a>
+  </div>
+  <div id="links">
+    <a href="https://www.alexandercardosi.com/eastern-dragon.php" title="Eastern Dragon Restaurant">Eastern Dragon</a>
+    <a href="https://www.alexandercardosi.com/skillsusa.php" title="SkillsUSA: Mimmo's Pizzeria">SkillsUSA</a>
+    <a href="https://www.alexandercardosi.com/cvdts.php" title="Champlain Valley Driver Training School">CVDTS</a>
+    <a href="https://www.alexandercardosi.com/belgen-cycles.php">Belgen Cycles</a>
+    <a href="https://www.alexandercardosi.com/accessibility.php" title="Accesibility Website Creation">Accessibility Project</a>
+    <a href="https://www.alexandercardosi.com/resume.php">Resume</a>
+    <a href="http://cawdvt.org/students/acardosi/portfolio/contact.php">Contact</a>
+  </div>
+  <div id="menu">
+    <i class="material-icons" id="nav-icon">menu</i>
+  </div>
+</nav>
+<div id="transparent-overlay"></div>
